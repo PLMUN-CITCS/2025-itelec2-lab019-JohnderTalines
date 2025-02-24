70d4f686a3e237f12f069dde967c92795eadf99a
JohnderTalines <talinesjohnder_bsit@plmun.edu.ph>
2025-02-25 00:57:59 +0800

    Submitting Python Week 05 - Laboratory # 19

--- grade_calculator_functions.py
+++ grade_calculator_functions.py
@@ -1,7 +1,8 @@
 def get_student_score():
+
     """Prompts the user to enter their score and returns it as a numerical type."""
 
-    try:
+        try:
     while True:
             score = float(input("Please enter your score: "))
             return score
@@ -9,13 +10,14 @@ def get_student_score():
             print("Invalid input. Please enter a numeric value.")
 
 def calculate_grade(score):
+
     """Determines the letter grade based on the given score."""
     
     Parameters:
     score (float): The score to evaluate.
 
     Returns:
-    str: The corresponding letter grade.
+    str: The corresponding letter grade."""
 
     if score >= 90:
         return 'A'
@@ -29,6 +31,7 @@ def calculate_grade(score):
         return 'F'
 
 def main():
+
     """Main program flow to get the student's score and calculate the grade."""
 
     score = get_student_score()
